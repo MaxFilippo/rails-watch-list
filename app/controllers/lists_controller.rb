6c273a2419140a83b99e365032568ac6d0106932
Maximilian Eich <maximilian.f.d.eich@gmail.com>
2022-08-19 15:55:22 +0200

    problem?

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,11 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show edit update destroy]
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    # @list = List.find(params[:id])
     @bookmark = Bookmark.new
   end
 
@@ -14,5 +15,17 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
+    @list.save
+    redirect_to list_path, status: :see_other
+  end
+
+  private
+
+  def list_params
+    
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
